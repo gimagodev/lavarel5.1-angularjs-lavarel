43aff80e763f1a9307fbd41d7b7af77d81df3162
gimagodev <gimagodev@gmail.com>
2017-02-26 20:54:21 -0300

    Service layer

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,9 @@
 
 namespace CodeProject\Http\Controllers;
 
-// use CodeProject\Entities\Client;
+
 use CodeProject\Repositories\ClientRepository;
-// use CodeProject\Repositories\ClientRepositoryEloquent;
+use CodeProject\Services\ClientService;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
@@ -14,9 +14,19 @@ class ClientController extends Controller
   */
   private $repository;
   
-   public  function __construct(ClientRepository $repository)
+  /**
+  * @var ClientService
+  */
+  private $service;
+
+  /**
+  * @param ClientRepository $repository
+  * @param ClientService $service
+  */
+   public  function __construct(ClientRepository $repository, ClientService $service)
    {
        $this->repository = $repository;
+       $this->service = $service;
    }
    
     public function index()
@@ -32,8 +42,9 @@ class ClientController extends Controller
     */
     public function store(Request $request)
     {
-        //return Client::create($request->all());
-        return $this->repository->create($request->all());
+
+        return $this->service->create($request->all());
+        // return $this->repository->create($request->all());
     }
 
 
@@ -50,11 +61,25 @@ class ClientController extends Controller
     }
 
     /**
+    * Update the specified resource from storage
+    *
+    * @param int $id
+    * @return Response 
+    */
+    public function update($id)
+    {
+        //Client::find($id)->delete();
+        $this->repository->find($id)->update();
+    }
+
+
+    /**
     * Remove the specified resource from storage
     *
     * @param int  $id
     * @return Response 
     */
+
     public function destroy($id)
     {
         //Client::find($id)->delete();
